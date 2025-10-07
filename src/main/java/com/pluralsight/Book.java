7f9250148fdf065bbd2c18ba403ef19159cc2015
IrynaDelimarska <delimarskaya19@gmail.com>
2025-10-07 08:49:36 -0700

    methods done in book class

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -3,19 +3,88 @@ package com.pluralsight;
 public class Book {
     // Book Class
 
-        private int id;
-        private String isbn;
-        private String title;
-        private boolean isCheckedOut;
-        private String checkedOutTo;
+    private int id;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String checkedOutTo;
 
-        // Constructor
-        public Book(int id, String isbn, String title) {
-            this.id = id;
-            this.isbn = isbn;
-            this.title = title;
-            this.isCheckedOut = false;
-            this.checkedOutTo = "";
-        }
-
+    // Constructor
+    public Book(int id, String isbn, String title) {
+        this.id = id;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
     }
+
+//getters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    // create methods
+    public void checkedOut(String name) {
+        this.isCheckedOut = true;
+        this.checkedOutTo = name;
+    }
+
+    public void checkedIn() {
+        this.isCheckedOut = false;
+        this.checkedOutTo = "";
+    }
+
+    // Display book information
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", isCheckedOut=" + isCheckedOut +
+                ", checkedOutTo='" + checkedOutTo + '\'' +
+                '}';
+    }
+}
+
+
